3641b2b17c5e3b80f1cdcea8f3727c9bab8cdadf
U-BSD\347814 <347814@SHS-10DEC49DF.beaverton.k12.or.us>
2019-09-11 10:33:30 -0700

    guessingGame

---
+++ guessingGame.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <stdlib.h>
+using namespace std;
+int main(){
+  srand(time(NULL));
+  int randomnumber = rand() % 100+1; //generates a random number between 1 and 100
+  cout << randomnumber;
+  
+ 
+
+
+
+
+
+}
