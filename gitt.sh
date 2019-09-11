e23d2900236b33de9ba36c8af535542de9e537a1
U-BSD\347814 <347814@SHS-10DEC49DF.beaverton.k12.or.us>
2019-09-11 10:45:11 -0700

    thanks carson

---
+++ gitt.sh
@@ -0,0 +1,3 @@
+git add .
+git commit -m "thanks carson"
+git push origin master
--- guessingGame.cpp
+++ guessingGame.cpp
@@ -4,8 +4,26 @@ using namespace std;
 int main(){
   srand(time(NULL));
   int randomnumber = rand() % 100+1; //generates a random number between 1 and 100
-  cout << randomnumber;
-  
+  cout << randomnumber; //prints out the generated number for testing purposes
+  int guesses = 0;
+  int guess = 0;
+  while(!(guess==randomnumber){
+      cout << "enter your guess:"<< endl;
+      cin >> guess;
+      if(guess>randomnumber){
+	cout << "Too High" << endl;
+	guesses++;
+
+      }
+      if(guess<randomnumber){
+	
+
+
+
+      }
+
+    }
+      
  
 
 
