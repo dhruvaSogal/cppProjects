6315e8a137983b10fcd9333c5e291ef656717004
U-BSD\347814 <347814@SHS-10DEC49DF.beaverton.k12.or.us>
2019-09-13 10:44:18 -0700

    thanks carson

--- guessingGame.cpp
+++ guessingGame.cpp
@@ -2,28 +2,67 @@
 #include <stdlib.h>
 using namespace std;
 int main(){
-  srand(time(NULL));
-  int randomnumber = rand() % 100+1; //generates a random number between 1 and 100
-  cout << randomnumber; //prints out the generated number for testing purposes
-  int guesses = 0;
-  int guess = 0;
-  while(!(guess==randomnumber){
-      cout << "enter your guess:"<< endl;
+  bool running = true;
+  while (running ==true){
+    cout << "Shall We Play a Game?"<< endl;
+    cout<< "I will pick a number between 1 and 100, you need to guess it"<<endl;
+     srand(time(NULL));
+     int randomnumber = rand() % 100+1; //generates a random number between 1 and 100
+     //  cout << randomnumber<< endl; //prints out the generated number for testing purposes
+    int guesses = 0; //tracks guesses
+    int guess = 0;  //player's gues
+    char play = 'y'; //to set a boolean to terminate the program
+  bool playing = true;
+  while(playing==true){
+    
+      cout << "enter your guess:"<< endl; 
       cin >> guess;
       if(guess>randomnumber){
-	cout << "Too High" << endl;
+	cout << "Too High" << endl;  //if too high
 	guesses++;
 
       }
       if(guess<randomnumber){
-	cout << "Too Low" <<endl;
+	cout << "Too Low" <<endl; //if too low
 	guesses++;
 
       }
+      if(guess==randomnumber){
+	guesses++;              //win scenario
+	cout << "You Got It"<<endl;
+	cout << "You took:"<<guesses << " guesses"<< endl;
+	cout << "Do you want to play again? Enter y or n"<< endl;
+	cin >> play;
+	if(play=='n'){
+	  //end program with a boolean
+	  playing = false;
+	  running = false;
 
+	}
+	if(play=='y'){
+	  //continue program with a boolean
+	  playing = false;
+	  running = true;
+
+
+	}
+
+
+	}
+	
+	
       }
+  } 
+  if(running == false){}
+}
 
-    }
+
+
+      
+
+      
+
+    
       
  
 
@@ -31,4 +70,3 @@ int main(){
 
 
 
-}
