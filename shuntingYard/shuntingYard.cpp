e5b07ed13c7a1cdb2abf7a2c05bc06fc7878cd86
U-BSD\347814 <347814@SHS-10DECBC5A.beaverton.k12.or.us>
2020-02-19 10:48:47 -0800

    shunting yard

--- shuntingYard/shuntingYard.cpp
+++ shuntingYard/shuntingYard.cpp
@@ -1,23 +1,50 @@
 #include <iostream>
 #include "node.h"
+#include "string.h"
 using namespace std;
+
+void enqueu(node* &head, node* current, char* x, node* tail);
+  node* dequeu(node* &head, node* current);
+  void push(node* &stackhead, node* current, node* &stacktail, char* x);
+  node* pop(node* stackhead, node* current, node* &stacktail);
+void readInput(node* head, node* tail);
+void printqueu(node* head, node* tail, node* current);
+
+
+
+
+
+
 int main(){
   node* head = NULL;
   node* tail = NULL;
   node* stackhead = NULL;
   node* stacktail = NULL;
-  char* read = new char[90];
+
   cout<<"Enter your math equation"<<endl;
-  cin.getline(read, 90);
+  readInput(head, tail);
+  cout<<"has not crashed"<<endl;
+  printqueu(head, tail, head);
+
+
   
 } 
 
 void enqueu(node* &head, node* current, char* x, node* tail){ //add things after head FIFO
-  if(current->getNext() != NULL){
+  
+  if(current == NULL){
+    node* temp = new node(x);
+    head = temp;
+    enqueu(head, head, x, tail);
+
+  }
+  
+  else if(current->getNext() != NULL){
+    cout<<"d"<<endl;
     enqueu(head, current->getNext(), x, tail);
 
   }
-  else{
+  else{ //end of list
     node* temp = new node(x);
     tail = temp;
     current->setNext(tail);
@@ -25,6 +52,10 @@ void enqueu(node* &head, node* current, char* x, node* tail){ //add things after
   }
   
 }
+
+
+
+
 node* dequeu(node* &head, node* current){
   node* k = head;
   head = head->getNext();
@@ -47,6 +78,9 @@ void push(node* &stackhead, node* current, node* &stacktail, char* x){ //FILO, a
 
   
 }
+
+
+
 node* pop(node* stackhead, node* current, node* &stacktail){
   //stack so last out so
   node* temp = stackhead;
@@ -55,20 +89,47 @@ node* pop(node* stackhead, node* current, node* &stacktail){
 
 }
 
+
+
+
 char peek(){
   //pop then push
   
 
 
 }
-void readInput(char* read){
+
+
+
+void readInput(node* head, node* tail){
   /*
  keep track of two tokens, until you find space then take in those and make a char array
  then token one goes to point after space
    */
-  char k;
-  int counter;
-  for(int i = 0; i< ? ; i++){
+  char* read = new char[90];
+  cin.getline(read, 90);
+  int size = strlen(read);
+  
+
+
+  for(int i = 0; i < size; i++){
+    if((int)read[i] >= 30 && (int)read[i] <= 39 && read[i+1] == ' ' ){
+      
+      char* temp = new char;
+      temp = &(read[i]);
+      
+      enqueu(head, head, temp, tail);
+      cout<<"test"<<endl;
+      
+
+    }
+    
+    
+
+
+  }
+  
+  
     /*
       if char[i] != ' ' char* array = char[i]+char[i+1]...
       once you hit a space then new char*
@@ -76,8 +137,20 @@ void readInput(char* read){
 
 
     
-  }
   
+  
+}
+void printqueu(node* head, node* tail, node* current){
+  if(current != NULL){
+    cout<<current->getToken()<<' ';
+    if(current != tail){
+      printqueu(head, tail, current->getNext());
+      
+    }
+    
+
+    } 
+
 }
 
 
