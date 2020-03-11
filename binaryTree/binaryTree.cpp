41df381734df23e70ce625966b83115175a0e941
U-BSD\347814 <347814@SHS-10DECBC5A.beaverton.k12.or.us>
2020-03-11 09:34:56 -0700

    binary

--- binaryTree/binaryTree.cpp
+++ binaryTree/binaryTree.cpp
@@ -8,21 +8,17 @@ struct node{
   int value;
 
 };
-void insert(int a , node* current, node* root);
+void insert(int a , node* current, node* &root);
 void read(node* root);
+void print(node* current);
 int main(){
+  cout<<"This Program Creates a Binary Search Tree"<<endl;
   node* root = new node;
   root->right = NULL;
   root->left = NULL;
   root->value = 0;        //0 used if node is empty as input must me 1 to 1000
-  
-
-
-
-
-
-
-
+  read(root);
+  print(root);
 
 
 
@@ -30,7 +26,7 @@ int main(){
 void read(node* root){
   cout<<"Enter 1 for console entry or 2 for file input"<<endl;
   int entry = 0;
-  cin.ignore();
+  //cin.ignore();
   cin>>entry;
   if(entry == 1){
     char* input = new char[100];
@@ -42,18 +38,12 @@ void read(node* root){
     file<<input;
     file.close();
     ifstream infile;
-    infile.open("fileqwer");
+    infile.open("treefile");
     int a;
     while(infile >> a){
-      
-
-      insert(a, root, root);
-
-
-
-
-      
-
+      if(a != 0){
+      insert(a, root, root);   
+      }
     }
 
   }
@@ -69,16 +59,13 @@ void read(node* root){
     while(infile >> a){
       //sort into tree
       //call a method and recursively find spot
-
     }
-
-
   }
-
 }//end read method
-
-void insert(int a , node* current, node* root){
-if(root->value == 0){ //if first node in tree
+void insert(int a , node* current, node* &root){
+  cout<<a<<endl;
+  cout<<current->value<<endl;
+  if(root->value == 0){ //if first node in tree
 	node* temp = new node;
 	temp->value = a;
 	root = temp;
@@ -96,7 +83,6 @@ if(root->value == 0){ //if first node in tree
       current->left = temp;
     }
 
-    
   }
   else{ //equal to or greater than
     if(current->right != 0){
@@ -118,4 +104,15 @@ if(root->value == 0){ //if first node in tree
 
 
  }
+}
+void print(node* current){
+  if(current->value == 0){
+    return;
+  }
+  print(current->left);
+  cout<<current->value<<',';
+  print(current->right);
+
+  
+
 }
